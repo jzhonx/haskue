2e352c67abb60468279c21d1eb4ac8b536264c9d
Julian Zhou <junzhou365@gmail.com>
2023-12-13 17:43:13 +0800

    Simplify the disjunction intermediate result

--- src/Eval.hs
+++ src/Eval.hs
@@ -19,13 +19,10 @@ type Path = [Selector]
 -- the first argument is the value that is being evaluated.
 data PendingValue = PendingValue ValueZipper [Path] ([Value] -> Value)
 
-data EvalResult
-  = PartialDefault Value
-  | Complete Value
-
-fromEvalResult :: EvalResult -> Value
-fromEvalResult (PartialDefault _) = Bottom "marked value should be associated with a disjunction"
-fromEvalResult (Complete v) = v
+-- fromEvalResult :: EvalResult -> Value
+-- fromEvalResult (PartialDefault _) = Bottom "marked value should be associated with a disjunction"
+-- fromEvalResult (Complete v) = v
+--
 
 -- | ValueCrumb is a pair of a name and an environment. The name is the name of the field in the parent environment.
 type ValueCrumb = (String, Value)
@@ -61,18 +58,14 @@ initEnv :: ValueZipper
 initEnv = (Top, [])
 
 eval :: (MonadError String m) => Expression -> m Value
-eval expr = fst <$> runStateT m initEnv
-  where
-    m = do
-      r <- eval' expr
-      return $ fromEvalResult r
+eval expr = fst <$> runStateT (eval' expr) initEnv
 
-eval' :: (MonadError String m, MonadState ValueZipper m) => Expression -> m EvalResult
+eval' :: (MonadError String m, MonadState ValueZipper m) => Expression -> m Value
 eval' (UnaryExprCons e) = evalUnaryExpr e
 eval' (BinaryOpCons op e1 e2) = evalBinary op e1 e2
 
-evalLiteral :: (MonadError String m, MonadState ValueZipper m) => Literal -> m EvalResult
-evalLiteral lit = fmap Complete (f lit)
+evalLiteral :: (MonadError String m, MonadState ValueZipper m) => Literal -> m Value
+evalLiteral = f
   where
     f (StringLit (SimpleStringLit s)) = return $ String s
     f (IntLit i) = return $ Int i
@@ -100,7 +93,7 @@ evalStructLit s = do
             LabelString ls -> ls
        in do
             v <- eval' e
-            return (name, fromEvalResult v)
+            return (name, v)
     unifySameKeys :: (MonadError String m, MonadState ValueZipper m) => [(String, Value)] -> m (Map.Map String Value)
     unifySameKeys fields = sequence $ Map.fromListWith (\mx my -> do x <- mx; y <- my; unify x y) (map (\(k, v) -> (k, return v)) fields)
 
@@ -111,42 +104,36 @@ evalStructLit s = do
     getVarLabels :: [(Label, Expression)] -> [String]
     getVarLabels xs = map (\(l, _) -> fromJust (fetchVarLabel l)) (filter (\(l, _) -> isJust (fetchVarLabel l)) xs)
 
-evalUnaryExpr :: (MonadError String m, MonadState ValueZipper m) => UnaryExpr -> m EvalResult
+evalUnaryExpr :: (MonadError String m, MonadState ValueZipper m) => UnaryExpr -> m Value
 evalUnaryExpr (PrimaryExprCons (Operand (Literal lit))) = evalLiteral lit
 evalUnaryExpr (PrimaryExprCons (Operand (OpExpression expr))) = eval' expr
 evalUnaryExpr (PrimaryExprCons (Operand (OperandName (Identifier ident)))) = lookupVar ident
 evalUnaryExpr (UnaryOpCons op e) = evalUnaryOp op e
 
-evalUnaryOp :: (MonadError String m, MonadState ValueZipper m) => UnaryOp -> UnaryExpr -> m EvalResult
-evalUnaryOp op e =
-  do
-    v <- evalUnaryExpr e
-    case v of
-      Complete v' -> evalVal v'
-      PartialDefault _ -> return $ Complete $ Bottom "marked value should be associated with a disjunction"
-  where
-    evalVal val = do
-      case (op, val) of
-        (Plus, Int i) -> return $ Complete $ Int i
-        (Minus, Int i) -> return $ Complete $ Int (-i)
-        (Star, _) -> return $ PartialDefault val
-        (Not, Bool b) -> return $ Complete $ Bool (not b)
-        _ -> throwError "evalUnaryOp: not correct type"
+evalUnaryOp :: (MonadError String m, MonadState ValueZipper m) => UnaryOp -> UnaryExpr -> m Value
+evalUnaryOp op e = do
+  val <- evalUnaryExpr e
+  case (op, val) of
+    (Plus, Int i) -> return $ Int i
+    (Minus, Int i) -> return $ Int (-i)
+    -- (Star, _) -> return $ PartialDefault val
+    (Not, Bool b) -> return $ Bool (not b)
+    _ -> throwError "evalUnaryOp: not correct type"
 
 -- order of arguments is important for disjunctions.
 -- left is always before right.
-evalBinary :: (MonadError String m, MonadState ValueZipper m) => BinaryOp -> Expression -> Expression -> m EvalResult
+evalBinary :: (MonadError String m, MonadState ValueZipper m) => BinaryOp -> Expression -> Expression -> m Value
+evalBinary AST.Disjunction e1 e2 = evalDisjunction e1 e2
 evalBinary op e1 e2 = do
-  r1 <- eval' e1
-  r2 <- eval' e2
-  evalRes op r1 r2
+  v1 <- eval' e1
+  v2 <- eval' e2
+  evalVal v1 v2
   where
-    evalRes AST.Disjunction r1 r2 = evalDisjunction r1 r2
-    evalRes _ r1 r2 =
-      let v1 = fromEvalResult r1
-          v2 = fromEvalResult r2
-       in fmap Complete (evalVal v1 v2)
-
+    -- evalRes AST.Disjunction r1 r2 = evalDisjunction r1 r2
+    -- evalRes _ r1 r2 =
+    --   let v1 = fromEvalResult r1
+    --       v2 = fromEvalResult r2
+    --    in fmap Complete (evalVal v1 v2)
     evalVal v1 v2 = do
       case (op, v1, v2) of
         (Unify, _, _) -> unify v1 v2
@@ -156,46 +143,67 @@ evalBinary op e1 e2 = do
         (Div, Int i1, Int i2) -> return $ Int (i1 `div` i2)
         _ -> throwError "evalBinary: not integers"
 
-evalDisjunction :: (MonadError String m, MonadState ValueZipper m) => EvalResult -> EvalResult -> m EvalResult
-evalDisjunction = f
+data DisjunctItem = DisjunctDefault Value | DisjunctRegular Value
+
+-- evalDisjunction is used to evaluate a disjunction.
+evalDisjunction :: (MonadError String m, MonadState ValueZipper m) => Expression -> Expression -> m Value
+evalDisjunction e1 e2 = case (e1, e2) of
+  (UnaryExprCons (UnaryOpCons Star e1'), UnaryExprCons (UnaryOpCons Star e2')) ->
+    evalExprPair (evalUnaryExpr e1') DisjunctDefault (evalUnaryExpr e2') DisjunctDefault
+  (UnaryExprCons (UnaryOpCons Star e1'), _) ->
+    evalExprPair (evalUnaryExpr e1') DisjunctDefault (eval' e2) DisjunctRegular
+  (_, UnaryExprCons (UnaryOpCons Star e2')) ->
+    evalExprPair (eval' e1) DisjunctRegular (evalUnaryExpr e2') DisjunctDefault
+  (_, _) -> evalExprPair (eval' e1) DisjunctRegular (eval' e2) DisjunctRegular
   where
-    f (PartialDefault v1) r2 =
+    -- evalExprPair is used to evaluate a disjunction with both sides still being unevaluated.
+    evalExprPair :: (MonadError String m, MonadState ValueZipper m) => m Value -> (Value -> DisjunctItem) -> m Value -> (Value -> DisjunctItem) -> m Value
+    evalExprPair m1 cons1 m2 cons2 = do
+      v1 <- m1
+      v2 <- m2
+      evalDisjPair (cons1 v1) (cons2 v2)
+
+    -- evalDisjPair is used to evaluate a disjunction whose both sides are evaluated.
+    evalDisjPair :: (MonadError String m, MonadState ValueZipper m) => DisjunctItem -> DisjunctItem -> m Value
+    evalDisjPair (DisjunctDefault v1) r2 =
       evalLeftPartial (\(df1, ds1, df2, ds2) -> newDisj df1 ds1 df2 ds2) v1 r2
     -- reverse v2 r1 and also the order to the disjCons.
-    f r1@(Complete _) (PartialDefault v2) =
+    evalDisjPair r1@(DisjunctRegular _) (DisjunctDefault v2) =
       evalLeftPartial (\(df2, ds2, df1, ds1) -> newDisj df1 ds1 df2 ds2) v2 r1
-    f (Complete v1) (Complete v2) = evalFullDisj v1 v2
+    evalDisjPair (DisjunctRegular v1) (DisjunctRegular v2) = evalRegularDisj v1 v2
 
-    -- evalLeftPartial is used to evaluate a disjunction whose left side is a partial default.
-    evalLeftPartial :: (MonadError String m) => (([Value], [Value], [Value], [Value]) -> m EvalResult) -> Value -> EvalResult -> m EvalResult
-    evalLeftPartial disjCons (Value.Disjunction df1 ds1) (Complete (Value.Disjunction _ ds2)) =
+    -- evalLeftPartial is used to evaluate a disjunction whose left side is a default.
+    -- the first argument is a function that takes the four lists of values and returns a disjunction.
+    evalLeftPartial :: (MonadError String m) => (([Value], [Value], [Value], [Value]) -> m Value) -> Value -> DisjunctItem -> m Value
+    evalLeftPartial disjCons (Value.Disjunction df1 ds1) (DisjunctRegular (Value.Disjunction _ ds2)) =
       -- This is rule M2 and M3. We eliminate the defaults from the right side.
       disjCons (df1, ds1, [], ds2)
-    evalLeftPartial disjCons v1 (Complete (Value.Disjunction df2 ds2)) =
+    evalLeftPartial disjCons v1 (DisjunctRegular (Value.Disjunction df2 ds2)) =
       -- This is rule M1.
       disjCons ([v1], [v1], df2, ds2)
-    evalLeftPartial disjCons v1 (Complete v2) =
+    evalLeftPartial disjCons v1 (DisjunctRegular v2) =
       disjCons ([v1], [v1], [], [v2])
-    evalLeftPartial disjCons v1 (PartialDefault v2) =
+    evalLeftPartial disjCons v1 (DisjunctDefault v2) =
       disjCons ([], [v1], [], [v2])
 
+    -- evalFullDisj is used to evaluate a disjunction whose both sides are regular.
     -- Rule: D1, D2
-    evalFullDisj (Value.Disjunction df1 ds1) (Value.Disjunction df2 ds2) = newDisj df1 ds1 df2 ds2
-    evalFullDisj (Value.Disjunction d ds) y = newDisj d ds [] [y]
-    evalFullDisj x (Value.Disjunction d ds) = newDisj [] [x] d ds
+    evalRegularDisj (Value.Disjunction df1 ds1) (Value.Disjunction df2 ds2) = newDisj df1 ds1 df2 ds2
+    evalRegularDisj (Value.Disjunction d ds) y = newDisj d ds [] [y]
+    evalRegularDisj x (Value.Disjunction d ds) = newDisj [] [x] d ds
     -- Rule D0
-    evalFullDisj x y = newDisj [] [x] [] [y]
+    evalRegularDisj x y = newDisj [] [x] [] [y]
 
     -- dedupAppend appends unique elements in ys to the xs list, but only if they are not already in xs.
     -- xs and ys are guaranteed to be unique.
     dedupAppend :: [Value] -> [Value] -> [Value]
     dedupAppend xs ys = xs ++ foldr (\y acc -> if y `elem` xs then acc else y : acc) [] ys
 
-    newDisj df1 ds1 df2 ds2 = return $ Complete $ Value.Disjunction (dedupAppend df1 df2) (dedupAppend ds1 ds2)
+    newDisj df1 ds1 df2 ds2 = return $ Value.Disjunction (dedupAppend df1 df2) (dedupAppend ds1 ds2)
 
-lookupVar :: (MonadError String m, MonadState ValueZipper m) => String -> m EvalResult
+lookupVar :: (MonadError String m, MonadState ValueZipper m) => String -> m Value
 lookupVar name = do
   z <- get
   case searchUp name z of
-    Just v -> return $ Complete v
+    Just v -> return v
     Nothing -> throwError $ "variable " ++ name ++ " is not defined"
