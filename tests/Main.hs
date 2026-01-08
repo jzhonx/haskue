e6f98a8a72ac02da90f2220b5a946c0dd7d411b5
Julian Zhou <junzhou365@gmail.com>
2026-01-08 18:24:32 +0800

    Remove unused tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,13 +1,9 @@
 module Main where
 
-import PropGraphTest (ngTests)
 import SpecTest (specTests)
 import Test.Tasty
-import TreeTest (treeTests)
 
 main :: IO ()
 main = do
   stests <- specTests
-  ntests <- ngTests
-  trtests <- treeTests
-  defaultMain $ testGroup "all_tests" [stests, ntests, trtests]
+  defaultMain $ testGroup "all_tests" [stests]
